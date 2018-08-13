804c57136f2ff37bacae7dc4104a82b2b64841f7
Jordan Parker <parkerj4321@gmail.com>
2018-08-13 10:52:54 -0500

    Everything but EX2

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("headerText").innerHTML = "Jordan";
+
+var nameAdd = document.getElementById("headerChanger").innerHTML;
+console.log(nameAdd + " Parker");
+nameAdd = " Parker";
+
+document.getElementById("kennButton").innerHTML= "Redo";
