b8fa17b09f41539fe530a6b5a484df56a2b23046
Jordan Parker <parkerj4321@gmail.com>
2018-08-13 10:59:42 -0500

    Finished EX2

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 document.getElementById("headerText").innerHTML = "Jordan";
 
 var nameAdd = document.getElementById("headerChanger").innerHTML;
-console.log(nameAdd + " Parker");
-nameAdd = " Parker";
+document.getElementById("headerChanger").innerText = nameAdd + " Parker";
 
 document.getElementById("kennButton").innerHTML= "Redo";
